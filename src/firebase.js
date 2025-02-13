ccde0baa6944af6ae9aa24ab091384b0df87228d
Franco <freconur@gmail.com>
2025-02-13 13:38:00 -0500

    no sabemos

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,11 @@
 const { initializeApp, cert } = require('firebase-admin/app')
+const express = require('express')
 // import { getFirestore,collection } from 'firebase/firestore'
+const app = express()
 const { getFirestore } = require('firebase-admin/firestore')
 const { getAuth } = require('firebase-admin/auth')
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 const dotenv = require('dotenv')
 dotenv.config()
 initializeApp({
